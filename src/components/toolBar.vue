22c82f6c98b6ed844d7f6894c995e0f4345ee063
itsMikeLowrey <itsMikeLowrey@users.noreply.github.com>
2019-09-05 01:35:53 -0400

    Added dark theme and removed template stuff

---
+++ src/components/toolBar.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <v-toolbar>
+
+      <v-toolbar-title>Distributed Keys</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+
+        <v-btn text
+        v-bind:input-value="page.title===current"
+          v-for="page in pages"
+          :key="page.title"
+          :href="page.link" >
+
+            {{page.title}}
+
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'toolBar',
+  props: {
+
+  },
+  data: () => ({
+    pages: [
+      { title: 'Home', link: 'https://www.multisigcrypto.com/' },
+      { title: 'Buy', link: '/' },
+      { title: 'Setup', link: '/' },
+      { title: 'Guides', link: '/' },
+      { title: 'About', link: 'https://www.multisigcrypto.com/about' }
+    ],
+    current: 'Test'
+  })
+}
+</script>
+
+<style scoped>
+a{text-decoration: none;
+color:white;}
+</style>
